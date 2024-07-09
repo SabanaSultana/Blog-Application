e839da9c53f333fd5074d7a3295e7a1bdca39160
Sabana <sabanasultana132@gmail.com>
2024-07-09 23:51:29 +0530

    added user controller

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,16 +1,16 @@
 const express=require("express")
-const { registerUser, login, getUsers } = require("../controllers/userController")
+const { registerUser, login, getAllUsers } = require("../controllers/userController")
 
 const router=express.Router()
 
 //register user
-router.post('signup',registerUser)
+router.post('/signup',registerUser)
 
 //login user
 router.post('/login',login)
 
 //get User
 
-router.get('/all-users',getUsers)
+router.get('/all-users',getAllUsers)
 
 module.exports=router
